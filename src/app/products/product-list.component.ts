d84daa69499c7570b2e0b55855ea6c75cca27890
Pallavi Dharmapuri <pdharmapuri@pordoceoed.com>
2022-12-19 21:34:33 -0600

    creating a service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector :'pm-products',
@@ -7,55 +8,28 @@ import { IProduct } from './product';
     styleUrls:['./product-list.component.css']
 })
 
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth : number = 50;
     imageMargin :number = 2;
     showImage : boolean = false;
-    filterBy :string = 'ham';
+    filterBy :string = '';
     //products : any
-    products : IProduct[] =[
-      {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2021",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "assets/images/leaf_rake.png"
-      },
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2021",
-        "description": "Leaf rake with 48-inch wooden handle. Leaf rake with 48-inch wooden handle.",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png"
-      },
-      {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2021",
-        "description": "Leaf rake with 48-inch wooden handle. Leaf rake with 48-inch wooden handle.Leaf rake with 48-inch wooden handle.",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "assets/images/hammer.png"
-      },
-      {
-        "productId": 8,
-        "productName": "Saw",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2021",
-        "description": "",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "assets/images/saw.png"
-      }]
+    products : IProduct[] | undefined ;
+    filteredProducts : IProduct[] | undefined ;
+    constructor(private _productService: ProductService){
+      this.products = _productService.getProducts();
+    }
+    ngOnInit(): void {
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
+    }
 
+    filterProducts(): void {
+      this.filterBy = this.filterBy.toLowerCase();
+      this.filteredProducts = this.products?.filter((product : IProduct) =>
+      product.productName.toLowerCase().includes(this.filterBy));
+    }
       toggleImage():void{
         this.showImage = !this.showImage;
       }
---
+++ src/app/products/product.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from "@angular/core";
+import { IProduct } from "./product";
+
+@Injectable({
+    providedIn : 'root'
+})
+export class ProductService{
+getProducts():IProduct[]{
+    return [
+        {
+          "productId": 1,
+          "productName": "Leaf Rake",
+          "productCode": "GDN-0011",
+          "releaseDate": "March 19, 2021",
+          "description": "Leaf rake with 48-inch wooden handle.",
+          "price": 19.95,
+          "starRating": 3.2,
+          "imageUrl": "assets/images/leaf_rake.png"
+        },
+        {
+          "productId": 2,
+          "productName": "Garden Cart",
+          "productCode": "GDN-0023",
+          "releaseDate": "March 18, 2021",
+          "description": "Leaf rake with 48-inch wooden handle. Leaf rake with 48-inch wooden handle.",
+          "price": 32.99,
+          "starRating": 4.2,
+          "imageUrl": "assets/images/garden_cart.png"
+        },
+        {
+          "productId": 5,
+          "productName": "Hammer",
+          "productCode": "TBX-0048",
+          "releaseDate": "May 21, 2021",
+          "description": "Leaf rake with 48-inch wooden handle. Leaf rake with 48-inch wooden handle.Leaf rake with 48-inch wooden handle.",
+          "price": 8.9,
+          "starRating": 4.8,
+          "imageUrl": "assets/images/hammer.png"
+        },
+        {
+          "productId": 8,
+          "productName": "Saw",
+          "productCode": "TBX-0022",
+          "releaseDate": "May 15, 2021",
+          "description": "",
+          "price": 11.55,
+          "starRating": 3.7,
+          "imageUrl": "assets/images/saw.png"
+        }];
+}
+}
