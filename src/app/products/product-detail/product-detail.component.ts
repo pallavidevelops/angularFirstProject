f7ac0bae868a385595ae392aa00ef4da31dce6a0
Pallavi D <pallavi.devel@gmail.com>
2022-12-25 01:46:50 -0700

    route to view using Router for button click action

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from '../product';
 
 @Component({
@@ -10,12 +10,16 @@ import { IProduct } from '../product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail page";
   product : IProduct | undefined;
-  constructor(private route : ActivatedRoute) { }
+  constructor(private route : ActivatedRoute, private router : Router) { }
 
   ngOnInit(): void {
     //Read route parameter
    const id : number = Number(this.route.snapshot.paramMap.get('id')) ;
    console.log('route id from Url',id);
+  }
+
+  backToProductListPage(): void{
+    this.router.navigate(['/products']);
   }
 
 }
