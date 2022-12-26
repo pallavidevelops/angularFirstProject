1614d6c0bef0d80c79b83c8b815d67fce2a00a8e
pallavidevelops <63525064+pallavidevelops@users.noreply.github.com>
2022-12-26 15:03:00 -0700

    changes from routing initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
-import { WelcomeComponent } from './home/welcome.component';
-import { FormsModule } from '@angular/forms';
-import { ProductModule } from './products/product.module';
-import { PageNotFoundComponent } from './page-not-found.component';
-import { MessageModule } from './messages/message.module';
-import { UserModule } from './user/user.module';
+import { HttpClientModule } from '@angular/common/http';
 
-const routes : Routes =  [
-  {path :'welcome', component: WelcomeComponent},
-  {path: '',redirectTo:'welcome', pathMatch:'full'}
-]
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ProductData } from './products/product-data';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+/* Feature Modules */
+import { ProductModule } from './products/product.module';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './messages/message.module';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
+    ProductModule,
+    UserModule,
+    MessageModule
+  ],
   declarations: [
     AppComponent,
     WelcomeComponent,
     PageNotFoundComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    RouterModule.forRoot(routes),
-    ProductModule,
-    UserModule,
-    MessageModule
-  ],
   bootstrap: [AppComponent]
 })
-
-export class AppModule { 
- 
-}
+export class AppModule { }
